2e1e57714e4aeca072892230a681df1d3e1e9bc1
Stanislaw <kulsarin07@gmail.com>
2025-03-05 00:44:57 -0800

    Добавление моделей для приложения users

--- backend/instagram/users/models.py
+++ backend/instagram/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class InstagramUser(AbstractUser):
+    """Модель пользователя"""
+
+    image = models.ImageField(upload_to='users_images/%Y/%m/%d/', default='users_images/depositphotos_133352152-stock-illustration-default-placeholder-profile-icon.jpg', blank=True)
+    status = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
