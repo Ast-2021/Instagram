9d825cc373bebf2c76fcf6839674f82e35ef273a
Stanislaw <kulsarin07@gmail.com>
2025-03-22 00:55:08 -0700

    [front] Добавления компонента для создания публикаций

---
+++ frontend/instagram/src/components/create-post-page/create-post.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { request_create_post } from "../../lib/requests";
+import "./create-post-style.css"
+
+function CreatePost() {
+    const [image, setImage] = useState(null);
+    const [description, setDescription] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+
+        formData.append('image', image);
+        formData.append('description', description);
+
+        try {
+          const response = await request_create_post({formData})
+          console.log(response.data)
+          navigate(`/post/${response.data.pk}/`);
+        } catch (error) {
+          setError('Failed to create post')
+          console.log(error)
+        };
+  };
+
+  return (
+    <div className="create">
+      <h2>Create Post</h2>
+      {error && <p className="error-text">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Image:</label>
+          <input
+            className="input-create-post-file"
+            type="file"
+            onChange={(e) => setImage(e.target.files[0])}
+            required
+          />
+        </div>
+        <div>
+          <label>Description:</label>
+          <input
+            className="input-create-post-description"
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreatePost;
